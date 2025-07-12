728a7c4f263f37601d50462416d09343764680a8
Mario Lucio Gomes Junior <126926485+mariomanj@users.noreply.github.com>
2025-07-11 21:44:41 -0300

    Add files via upload

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,7 @@
+x = y = z = 0
+
+x = int(input("Digite um numero: "))
+y = int(input("Digite outro numero: "))
+
+z = x + y
+print('A soma dos valores é:', z)
---
+++ variaveis.py
@@ -0,0 +1,22 @@
+media = 0
+n1 = n2 = n3 = n4 = 0.0
+nome, idade = 'Mario', 33
+estado = True
+
+# Função type()
+#print(type(media))
+#print(type(n1))
+#print(type(nome))
+#print(type(estado))
+#print(type(1+2j))
+
+# Função isinstance()
+#a = 20
+#b = 'mario'
+#print(isinstance(a, (int, float)))
+#print(isinstance(b, int))
+
+a = 40
+c = 5
+r = a * c
+print(r)
